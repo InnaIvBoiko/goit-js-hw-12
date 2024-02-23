0866ed3cf648c4ead39a47786146303339854428
InnaIvBoiko <inna_boiko@libero.it>
2024-02-23 17:48:32 +0100

    add axios and btn "Load more", update pixabay.js with axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,7 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import axios from 'axios';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 import ErrorIcon from '../img/error.svg';
 
@@ -9,12 +11,14 @@ const API_KEY = '42438077-634a4b32cfcaa96ebaa8c4719';
 const API_URL = 'https://pixabay.com/api/';
 
 async function searchImg() {
+    const loading = document.querySelector('.loading');
     const QUERY = encodeURIComponent(keywords.value);
-    const URL = `${API_URL}?key=${API_KEY}&q=${QUERY}&image_type=photo&orientation=horizontal&safesearch=true`;
+    const URL = `${API_URL}?key=${API_KEY}&q=${QUERY}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15`;
     
-    const response = await fetch(URL);
-    const data = await response.json();
-    return data;
+    const response = await axios.get(URL);
+    loading.classList.add('visually-hidden'); 
+
+    return response.data;
 };
 
 const errorMessage = () =>
--- src/main.js
+++ src/main.js
@@ -2,34 +2,41 @@ import { keywords, searchImg, errorMessage } from './js/pixabay-api';
 import { gallery, markUp } from "./js/render-functions";
 
 const formSearch = document.querySelector('.form-search');
+const showMoreBtn = document.querySelector('.btn-more');
+let page = 1;
 
 formSearch.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const loading = document.querySelector('.loading')
+    const loading = document.querySelector('.loading');
     gallery.innerHTML = '';
+    page = 1;
     
     if (keywords.value) {
         loading.classList.remove('visually-hidden');
-        setTimeout(() => {
-            loading.classList.add('visually-hidden');            
-        }, 1000); 
-        setTimeout(() => { 
-            searchImg()
-                .then(data => {
-                    if (parseInt(data.totalHits) > 0) {
-                        return data.hits;
-                    };
-                })
-                .then((images) => {
-                    markUp(images);
-                })
-                .catch(() => {
-                    errorMessage();
-                })
-                .finally(() => {
-                    formSearch.reset();
-                });
-        }, 1000);
-    };   
+
+        searchImg()
+            .then(data => {
+                if (parseInt(data.totalHits) > 0) {
+                    showMoreBtn.classList.remove('visually-hidden');
+                    return data.hits;
+                };
+            })
+            .then((images) => {
+                markUp(images);
+            })
+            .catch(() => {
+                errorMessage();
+            })
+            .finally(() => {
+                formSearch.reset();
+            });
+    };
+   
 });
+
+showMoreBtn.addEventListener('click', () => {
+    page++;
+    
+});
+
